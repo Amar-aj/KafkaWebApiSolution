c65f3290b4e008405480d8a0d96989f755bd7fe2
Amar-aj <amar.aj.github@gmail.com>
2024-11-11 22:10:23 +0530

    complete

---
+++ ConsumerWebApi/ConsumerService.cs
@@ -0,0 +1,57 @@
+﻿
+using Confluent.Kafka;
+
+namespace ConsumerWebApi;
+
+public class ConsumerService : BackgroundService
+{
+    private readonly IConfiguration _configuration;
+    private readonly IConsumer<Ignore, string> _consumer;
+
+    private readonly ILogger<ConsumerService> _logger;
+
+    readonly string _topic = "ProduceChat";
+
+    public ConsumerService(IConfiguration configuration, ILogger<ConsumerService> logger)
+    {
+        _logger = logger;
+        _configuration = configuration;
+
+        var consumerConfig = new ConsumerConfig
+        {
+            BootstrapServers = _configuration["Kafka:BootstrapServers"],
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            GroupId = "my-group",
+        };
+        _consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _consumer.Subscribe(_topic);
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            ProcessKafkaMessage(stoppingToken);
+
+            Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+        }
+
+        _consumer.Close();
+    }
+
+    public void ProcessKafkaMessage(CancellationToken stoppingToken)
+    {
+        try
+        {
+            var consumeResult = _consumer.Consume(stoppingToken);
+
+            var message = consumeResult.Message.Value;
+
+            _logger.LogInformation($"Received chat update: {message}");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error processing Kafka message: {ex.Message}");
+        }
+    }
+}
---
+++ ProducerWebApi/Controllers/ProducerController.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProducerWebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProducerController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    private readonly IProducer<Null, string> _producer;
+    readonly string _topic = "ProduceChat";
+    readonly string _clientId = "my-app";
+
+    public ProducerController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+
+        var producerconfig = new ProducerConfig
+        {
+            BootstrapServers = _configuration["Kafka:BootstrapServers"],
+            ClientId = _clientId,
+            BrokerAddressFamily = BrokerAddressFamily.V4,
+        };
+        _producer = new ProducerBuilder<Null, string>(producerconfig).Build();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(string message)
+    {
+        var kafkamessage = new Message<Null, string> { Value = message, };
+        await _producer.ProduceAsync(_topic, kafkamessage);
+        return Ok("Updated Successfully...");
+    }
+}
